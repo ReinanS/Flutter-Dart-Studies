6fcda3b6fea3dd0c178dd9cbee6e78bcf642db37
reinanS <reinandesouza01@gmail.com>
2021-04-02 11:39:02 -0300

    ref: remove singleScrowView

--- Flutter/flutter_login_signup_ui/lib/Screens/Home/components/body.dart
+++ Flutter/flutter_login_signup_ui/lib/Screens/Home/components/body.dart
@@ -8,29 +8,30 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  Future<List<Produto>> produtos = ProdutosAPI.fetchProdutos();
+  Future<List<Produto>> produtos;
+
+  @override
+  void initState() {
+    super.initState();
+    produtos = ProdutosAPI.fetchProdutos();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: FutureBuilder(
-        future: produtos,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text("Erro ao acessar os dados"));
-          }
+    return FutureBuilder(
+      future: produtos,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(child: Text("Erro ao acessar os dados"));
+        }
 
-          if (!snapshot.hasData) {
-            return Center(child: CircularProgressIndicator());
-          }
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
 
-          List<Produto> produtos = snapshot.data;
-
-          print(produtos[0].nome);
-
-          return _listView(produtos);
-        },
-      ),
+        List<Produto> produtos = snapshot.data;
+        return _listView(produtos);
+      },
     );
   }
 
