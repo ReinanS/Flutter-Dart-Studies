47087a7ca61eb81da09248254c2985d9228d0adb
reinanS <reinandesouza01@gmail.com>
2021-04-04 10:37:42 -0300

    style: atualiza constantes

--- Flutter/autentication_login/lib/constants.dart
+++ Flutter/autentication_login/lib/constants.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
 
+const kBackgroundColor = Color(0xFFF1EFF1);
 const kPrimaryColor = Color(0xFF6F35A5);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
+const kSecondColor = Color(0xFFFFA41B);
+const kTextColor = Color(0xFF000839);
+const kTextLightColor = Color(0xFF747474);
+const kBlueColor = Color(0xFF40BAD5);
+
+const kDefaultPadding = 20.0;
+
+const kDefaultShadow = BoxShadow(
+  offset: Offset(0, 15),
+  blurRadius: 27,
+  color: Colors.black12, // Black color with 12% opacity
+);
