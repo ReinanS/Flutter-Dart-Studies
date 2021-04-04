5d9b983cd542bf205a13665f00de49320ab4de51
reinanS <reinandesouza01@gmail.com>
2021-04-04 11:11:34 -0300

    feat: adiciona category list widget

---
+++ Flutter/autentication_login/lib/Screens/product/components/category_list.dart
@@ -0,0 +1,52 @@
+import 'package:autentication_login/constants.dart';
+import 'package:flutter/material.dart';
+
+class CategoryList extends StatefulWidget {
+  @override
+  _CategoryListState createState() => _CategoryListState();
+}
+
+class _CategoryListState extends State<CategoryList> {
+  int selectedIndex = 0;
+  List categories = ['All', 'Cell phone', 'TV', 'Sofa', 'Armchair'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: kDefaultPadding / 2), // 5
+      height: 30,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: () {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          child: Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(
+              left: kDefaultPadding,
+              // At end item it add extra 20 right padding
+              right: index == categories.length - 1 ? kDefaultPadding : 0,
+            ),
+            padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            decoration: BoxDecoration(
+              color: index == selectedIndex
+                  ? Colors.white.withOpacity(0.4)
+                  : Colors.transparent,
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Text(
+              categories[index],
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
