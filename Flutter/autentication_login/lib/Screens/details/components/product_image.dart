ba3e15c2d902f683cde0e7a489990de4e6e6ae48
reinanS <reinandesouza01@gmail.com>
2021-04-08 15:02:05 -0300

    feat: adiciona component product_image

---
+++ Flutter/autentication_login/lib/Screens/details/components/product_image.dart
@@ -0,0 +1,39 @@
+import 'package:autentication_login/constants.dart';
+import 'package:flutter/material.dart';
+
+class ProductPoster extends StatelessWidget {
+  ProductPoster({
+    this.size,
+    this.image,
+  });
+
+  Size size;
+  String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: kDefaultPadding),
+      height: size.height * 0.8,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: <Widget>[
+          Container(
+            height: size.height * 0.7,
+            width: size.width * 0.7,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+            ),
+          ),
+          Image.network(
+            image,
+            height: size.height * 0.75,
+            width: size.width * 0.75,
+            fit: BoxFit.cover,
+          )
+        ],
+      ),
+    );
+  }
+}
