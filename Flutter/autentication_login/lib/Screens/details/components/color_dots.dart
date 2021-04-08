b8c903bbfb1a9818db901ea1573b5b3f81037b39
reinanS <reinandesouza01@gmail.com>
2021-04-08 15:17:23 -0300

    feat: adiciona component color  dot

---
+++ Flutter/autentication_login/lib/Screens/details/components/color_dots.dart
@@ -0,0 +1,34 @@
+import 'package:autentication_login/constants.dart';
+import 'package:flutter/material.dart';
+
+class ColorDot extends StatelessWidget {
+  ColorDot({
+    this.fillColor,
+    this.isSelected = false,
+  });
+
+  final Color fillColor;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: kDefaultPadding / 2.5),
+      padding: EdgeInsets.all(3),
+      height: 24,
+      width: 24,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: isSelected ? Color(0xFF707070) : Colors.transparent,
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: fillColor,
+        ),
+      ),
+    );
+  }
+}
