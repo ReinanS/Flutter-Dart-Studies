6edc3448fd8c7cda45e01255e4575e0a612efd7f
reinanS <reinandesouza01@gmail.com>
2021-04-08 15:34:22 -0300

    feat: adiciona componente chat and add to cart

---
+++ Flutter/autentication_login/lib/Screens/details/components/chat_and_add_to_cart.dart
@@ -0,0 +1,49 @@
+import 'package:autentication_login/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class ChatAndAddToCart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(kDefaultPadding),
+      padding: EdgeInsets.symmetric(
+        horizontal: kDefaultPadding,
+        vertical: kDefaultPadding / 2,
+      ),
+      decoration: BoxDecoration(
+        color: Color(0xFFFCBF1E),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Row(
+        children: <Widget>[
+          FaIcon(
+            FontAwesomeIcons.comment,
+            size: 12,
+          ),
+          SizedBox(
+            width: kDefaultPadding / 2,
+          ),
+          Text(
+            "Chat",
+            style: TextStyle(color: Colors.white),
+          ),
+          Spacer(),
+          TextButton.icon(
+            onPressed: () {},
+            icon: FaIcon(
+              FontAwesomeIcons.shoppingCart,
+              size: 12,
+            ),
+            label: Text(
+              "Add to Cart",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
