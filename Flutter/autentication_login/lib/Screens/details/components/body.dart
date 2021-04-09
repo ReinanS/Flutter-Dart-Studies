5f9bbf047f0b366425c8a968e80514be930644df
reinanS <reinandesouza01@gmail.com>
2021-04-09 20:24:17 -0300

    feat: adiciona body

---
+++ Flutter/autentication_login/lib/Screens/details/components/body.dart
@@ -0,0 +1,94 @@
+import 'package:autentication_login/Screens/details/components/chat_and_add_to_cart.dart';
+import 'package:autentication_login/Screens/details/components/list_of_colors.dart';
+import 'package:autentication_login/Screens/details/components/product_image.dart';
+import 'package:autentication_login/constants.dart';
+import 'package:autentication_login/models/produto.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class Body extends StatefulWidget {
+  Produto produto;
+
+  Body({
+    this.produto,
+  });
+
+  @override
+  _BodyState createState() => _BodyState(produto: produto);
+}
+
+class _BodyState extends State<Body> {
+  Produto produto;
+
+  _BodyState({
+    this.produto,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return SafeArea(
+      bottom: false,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              decoration: BoxDecoration(
+                color: kBackgroundColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                    child: Hero(
+                      tag: '${produto.produtoId}',
+                      child: ProductPoster(
+                        size: size,
+                        image: produto.imagemUrl,
+                      ),
+                    ),
+                  ),
+                  ListOfColors(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: kDefaultPadding / 2),
+                    child: Text(
+                      produto.nome,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ),
+                  Text(
+                    'R\$ ${produto.preco}',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: kSecondColor,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+                    child: Text(
+                      produto.descricao,
+                      style: TextStyle(color: kTextLightColor),
+                    ),
+                  ),
+                  SizedBox(height: kDefaultPadding),
+                ],
+              ),
+            ),
+            ChatAndAddToCart(),
+          ],
+        ),
+      ),
+    );
+  }
+}
