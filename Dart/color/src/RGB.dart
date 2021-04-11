b725508cb0f94d561aa7a9f27d79d1cdd0e09441
reinanS <reinandesouza01@gmail.com>
2021-04-11 20:32:19 -0300

    feat: adiciona novos get e sets

---
+++ Dart/color/src/RGB.dart
@@ -0,0 +1,29 @@
+class RGB {
+  int _red = 0;
+  int _blue = 0;
+  int _green = 0;
+
+  int get red {
+    return _red;
+  }
+
+  set red(int red) {
+    this._red = red;
+  }
+
+  int get blue {
+    return _blue;
+  }
+
+  set blue(int blue) {
+    this._blue = blue;
+  }
+
+  int get green {
+    return _green;
+  }
+
+  set green(int green) {
+    this._green = green;
+  }
+}
