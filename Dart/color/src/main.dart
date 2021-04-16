ed8379d222ec7c4ad724dadd4aa2c9aae3e2d13e
reinanS <reinandesouza01@gmail.com>
2021-04-16 16:54:57 -0300

    feat: main

--- Dart/color/src/main.dart
+++ Dart/color/src/main.dart
@@ -1,1 +1,14 @@
-void main() {}
+import 'RGB.dart';
+
+void main() {
+  RGB rgb = RGB(37, 150, 190);
+
+  print(rgb.red);
+  print(rgb.green);
+  print(rgb.blue);
+
+  rgb.gray();
+  print(rgb.red);
+  print(rgb.green);
+  print(rgb.blue);
+}
