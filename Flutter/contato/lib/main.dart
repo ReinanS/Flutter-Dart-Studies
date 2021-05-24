d0b443d06276c4de66b6279cc2d42448b6aa8121
reinanS <reinandesouza01@gmail.com>
2021-05-24 19:22:17 -0300

    ref: referencia da HomePage no main

--- Flutter/contato/lib/main.dart
+++ Flutter/contato/lib/main.dart
@@ -1,10 +1,9 @@
+import 'package:contatos/views/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
