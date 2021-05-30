e75b5069681a9d2a40f58c789e91a2fa4ea28e85
reinanS <reinandesouza01@gmail.com>
2021-05-30 12:03:07 -0300

    feat: habilita cloudStore à aplicação

--- Flutter/cadastro/lib/main.dart
+++ Flutter/cadastro/lib/main.dart
@@ -1,5 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp());
+// TO-DO
+// Atualizar chamada da instancia do Firestore de forma apropriada
+// Fazer um cadastro de livros
+
+Future<void> main() async {
+  runApp(MaterialApp(
+    title: "Cadastro",
+    debugShowCheckedModeBanner: false,
+    home: Container(),
+  ));
+
+  // var db = Firestore.instance;
+
+  // await db.collection("produtos").snapshots().listen((resultado) {
+  //   resultado.documents.forEach((d) {
+  //     print(d.data);
+  //   });
+  // });
+
+  // db.collection("produtos").document("produto1").setData({
+  //   "nome": "cadeno",
+  //   "preço": 10.99,
+  //   "ativo": true,
+  // });
+
+  // db.collection("produtos").document("produto1").updateData({
+  //   "nome": "lápis",
+  //   "preço": 10.99,
+  //   "ativo": true,
+  // });
+
+  // QuerySnapshot resultado;
+  // resultado = await db.collection("produtos").getDocuments();
+
+  // resultado.documents.forEach((d) {
+  //   // print(d.documentID);
+  //   // print(d.data);
+
+  //   d.reference.updateData({
+  //     "ativo": false,
+  //   });
+  // });
+
+  // DocumentSnapshot resultadoDocumento =
+  //     await db.collection("produtos").document("produto1").get();
+
+  // print(resultadoDocumento.documentID);
+  // print(resultadoDocumento.data);
 }
